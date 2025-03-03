299169ea2ac255614ad4f15480128245368503a2
arya patel <aryaunjha@gmail.com>
2025-03-03 13:58:10 +0530

    for batter ui changes into nevbar

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center w-full px-6 py-4 bg-white shadow-md">
+    <nav className="flex justify-between items-center w-full">
       <div className="logo">
         <Link to="/">
-          <img src="https://res.cloudinary.com/dzsvjyg2c/image/upload/v1738649519/cinn4jbfs4fpl6uq9tqn.png" alt="Convict Logo" className="w-32" />
+          <img src="https://res.cloudinary.com/dzsvjyg2c/image/upload/v1738649519/cinn4jbfs4fpl6uq9tqn.png" alt="Convict Logo" className="w-[120px] h-auto" />
         </Link>
       </div>
-      <div className="flex gap-6 text-lg font-medium">
+      <div className="flex gap-5 text-[1.3rem] m-0 p-0">
         <Link to="/" className="hover:underline">Home</Link>
         <Link to="/internship" className="hover:underline">Internship</Link>
         <Link to="/job" className="hover:underline">Job</Link>
@@ -17,8 +17,8 @@ const Navbar = () => {
         <Link to="/contactus" className="hover:underline">Contact Us</Link>
       </div>
 
-      <div>
-        <Link to="/login" className="text-black text-[1.3rem] hover:underline ">
+      <div className='pr-[10px]'>
+        <Link to="/login" className=" text-black text-[1.3rem]  hover:underline ">
           Log in
         </Link>
       </div>
