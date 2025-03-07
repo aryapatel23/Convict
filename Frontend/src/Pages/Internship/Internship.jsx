bf68418a0bbc7f02d8b7a66dee03e26307475e93
arya patel <aryaunjha@gmail.com>
2025-03-07 16:31:20 +0530

    set route for internship and add filter internship component

--- Frontend/src/Pages/Internship/Internship.jsx
+++ Frontend/src/Pages/Internship/Internship.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Internship.css'
+import Filter_Internship from './Filter_Internships/Filter_Internship'
 
 const Internship = () => {
   return (
-    <div>
-      
+    <div className='max-w-full'>
+      <Filter_Internship/>
     </div>
   )
 }
