18588cef7e8d1dac5c8e71e06d31be8467098645
arya patel <aryaunjha@gmail.com>
2025-03-07 15:41:04 +0530

    add hover and on click effect in nevbar

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center w-full">
       <div className="logo">
-        <Link to="/">
+        <NavLink to="/">
           <img src="https://res.cloudinary.com/dzsvjyg2c/image/upload/v1738649519/cinn4jbfs4fpl6uq9tqn.png" alt="Convict Logo" className="w-[120px] h-auto" />
-        </Link>
+        </NavLink>
       </div>
       <div className="flex gap-5 text-[1.3rem] m-0 p-0">
-        <Link to="/" className="hover:underline">Home</Link>
-        <Link to="/internship" className="hover:underline">Internship</Link>
-        <Link to="/job" className="hover:underline">Job</Link>
-        <Link to="/aboutus" className="hover:underline">About Us</Link>
-        <Link to="/contactus" className="hover:underline">Contact Us</Link>
+        <NavLink 
+          to="/" 
+          className={({ isActive }) => isActive ? "font-bold underline" : "hover:underline"}
+        >
+          Home
+        </NavLink>
+        <NavLink 
+          to="/internship" 
+          className={({ isActive }) => isActive ? "font-bold underline" : "hover:underline"}
+        >
+          Internship
+        </NavLink>
+        <NavLink 
+          to="/job" 
+          className={({ isActive }) => isActive ? "font-bold underline" : "hover:underline"}
+        >
+          Job
+        </NavLink>
+        <NavLink 
+          to="/aboutus" 
+          className={({ isActive }) => isActive ? "font-bold underline" : "hover:underline"}
+        >
+          About Us
+        </NavLink>
+        <NavLink 
+          to="/contactus" 
+          className={({ isActive }) => isActive ? "font-bold underline" : "hover:underline"}
+        >
+          Contact Us
+        </NavLink>
       </div>
 
       <div className='pr-[10px]'>
-        <Link to="/login" className=" text-black text-[1.3rem]  hover:underline ">
+        <NavLink 
+          to="/login" 
+          className={({ isActive }) => isActive ? "text-black font-bold underline" : "text-black hover:underline"}
+        >
           Log in
-        </Link>
+        </NavLink>
       </div>
     </nav>
   );
---
+++ Frontend/src/Pages/Internship/Internship.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Internship.css'
+
+const Internship = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Internship
